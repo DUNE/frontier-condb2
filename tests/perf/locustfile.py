3dd629db516e64ef314b9f002672a616672d5414
Mike Clymer <mike.clymer@colostate.edu>
2026-01-29 13:28:59 -0700

    14 - Added different load test endpoints and local cache test config.

--- tests/perf/locustfile.py
+++ tests/perf/locustfile.py
@@ -1,18 +1,28 @@
 import os
 import subprocess
+import sys
 import time
+from typing import Any, Callable
 
 from locust import User, task
+
+CONDB2_SERVER_URL = "https://dbdata0vm.fnal.gov:9443/dune_runcon_prod"
+FN_FILEGET_PATH = "./clients/frontier/client/fn-fileget"
+FRONTIER_CLIENT_LD_LIBRARY_PATH = "/home/mike/dev/frontier/client"
+FRONTIER_PROXY_URL = "http://localhost:3128"
+FRONTIER_SERVER_URL = "http://fermicloud725.fnal.gov:8000/dune_runcon_prod"
 
 
 class SubprocessClient:
     def __init__(self, request_event) -> None:
         self._request_event = request_event
 
-    def __getattr__(self, name):
-        def wrapper(*args):
-            request_meta = {
-                "request_type": "subprocess.run()",
+    def __getattr__(self, name) -> Callable[..., Any | str]:
+        def wrapper(*args) -> Any | str:
+            _env = os.environ.copy()
+            _env["LD_LIBRARY_PATH"] = FRONTIER_CLIENT_LD_LIBRARY_PATH
+            _request_meta = {
+                "request_type": f"{args}",
                 "name": name,
                 "start_time": time.time(),
                 "response_length": 0,
@@ -20,31 +30,44 @@ class SubprocessClient:
                 "context": {},
                 "exception": None,
             }
-            start_perf_counter = time.perf_counter()
+            _result = None
+            _start_perf_counter = time.perf_counter()
 
-            _env = os.environ.copy()
-            _env["LD_LIBRARY_PATH"] = "/home/mike/dev/frontier/client"
-            print(f"_env['LD_LIBRARY_PATH']: {_env['LD_LIBRARY_PATH']}")
-            result = subprocess.run(
-                [*args],
-                env=_env,
-                capture_output=True,
-                shell=True,
-                text=True,
-            )
+            try:
+                _result = subprocess.run(
+                    [*args],
+                    env=_env,
+                    capture_output=True,
+                    check=True,
+                    shell=True,
+                    text=True,
+                )
 
-            print(f"STDOUT: {result.stdout}\n")
+                if (
+                    not _result.stdout
+                    or len(_result.stdout) == 0
+                    or _result.stdout == ""
+                ):
+                    _request_meta["exception"] = "No content in the response."
+                    sys.stdout.write(
+                        f"request_meta['exception']: {_request_meta['exception']}\n"
+                    )
+                else:
+                    _request_meta["response"] = _result.stdout
+                    sys.stdout.write(
+                        f"request_meta['response']: {_request_meta['response']}\n"
+                    )
+            except subprocess.CalledProcessError as cpe:
+                sys.stdout.write(
+                    f"Subprocess call returned a non-zero value: {cpe.returncode}\n{cpe.stderr}\n{cpe}"
+                )
+                _request_meta["exception"] = cpe
 
-            if result.returncode != 0:
-                request_meta["exception"] = result.stdout
-            else:
-                request_meta["response"] = result.stdout
-
-            request_meta["response_time"] = (
-                time.perf_counter() - start_perf_counter
+            _request_meta["response_time"] = (
+                time.perf_counter() - _start_perf_counter
             ) * 1000
-            self._request_event.fire(**request_meta)
-            return request_meta["response"]
+            self._request_event.fire(**_request_meta)
+            return _request_meta["response"]
 
         return wrapper
 
@@ -59,13 +82,47 @@ class SubProcessUser(User):
 
 class ConditionsDataUser(SubProcessUser):
     # @task
-    # def curl_query_cdb(self) -> None:
+    # def pd_vd_curl_query(self) -> None:
     #     self.client.curl_query_cdb(
-    #         'curl "https://dbdata0vm.fnal.gov:9443/dune_runcon_prod/get?folder=pdunesp.test&t=0"'
+    #         f"curl '{CONDB2_SERVER_URL}/get?folder=pdunesp.run_conditionstest&t=25034'"
+    #     )
+    #     self.client.curl_query_cdb(
+    #         f"curl '{CONDB2_SERVER_URL}/get?folder=pdunesp.run_conditionstest&t0=25100&t1=25115'"
+    #     )
+    #     self.client.curl_query_cdb(
+    #         f"curl '{CONDB2_SERVER_URL}/get?folder=pdunesp.run_conditionstest&t0=28650&t1=28655'"
+    #     )
+    #     self.client.curl_query_cdb(
+    #         f"curl '{CONDB2_SERVER_URL}/get?folder=pdunesp.run_conditionstest&t0=39252&t1=40260'"
     #     )
 
     @task
-    def fnfileget_query_cdb(self) -> None:
-        self.client.fnfileget_query_cdb(
-            './clients/frontier/client/fn-fileget -c "(serverurl=http://fermicloud725.fnal.gov:8000/dune_runcon_prod)(proxyurl=http://localhost:3128)" "get?folder=pdunesp.run_conditionstest&t0=25100&t1=25115"',
+    def pd_vd_fnfileget_query(self) -> None:
+        self.client.pd_vd_fnfileget_query(
+            f"{FN_FILEGET_PATH} -c '(serverurl={FRONTIER_SERVER_URL})(proxyurl={FRONTIER_PROXY_URL})' 'get?folder=pdunesp.run_conditionstest&t=25034'",
         )
+        self.client.pd_vd_fnfileget_query(
+            f"{FN_FILEGET_PATH} -c '(serverurl={FRONTIER_SERVER_URL})(proxyurl={FRONTIER_PROXY_URL})' 'get?folder=pdunesp.run_conditionstest&t0=25100&t1=25115'",
+        )
+        self.client.pd_vd_fnfileget_query(
+            f"{FN_FILEGET_PATH} -c '(serverurl={FRONTIER_SERVER_URL})(proxyurl={FRONTIER_PROXY_URL})' 'get?folder=pdunesp.run_conditionstest&t0=28650&t1=28655'",
+        )
+        self.client.pd_vd_fnfileget_query(
+            f"{FN_FILEGET_PATH} -c '(serverurl={FRONTIER_SERVER_URL})(proxyurl={FRONTIER_PROXY_URL})' 'get?folder=pdunesp.run_conditionstest&t0=39252&t1=40260'",
+        )
+
+    # @task
+    # def pd_2x2_fnfileget_query(self) -> None:
+    # curl "https://dbdata0vm.fnal.gov:9443/dune_runcon_prod/get?folder=neardet2x2.gain&t=0"
+    # curl "https://dbdata0vm.fnal.gov:9443/dune_runcon_prod/get?folder=neardet2x2.elifetime&t=0"
+    # curl "https://dbdata0vm.fnal.gov:9443/dune_runcon_prod/get?folder=neardet2x2.vdrift&t=0"
+
+    # self.client.pd_2x2_fnfileget_query(
+    #     f"{FN_FILEGET_PATH} -c '(serverurl={FRONTIER_SERVER_URL})(proxyurl={FRONTIER_PROXY_URL})' 'get?folder=neardet2x2.gain&t=0'",
+    # )
+    # self.client.pd_2x2_fnfileget_query(
+    #     f"{FN_FILEGET_PATH} -c '(serverurl={FRONTIER_SERVER_URL})(proxyurl={FRONTIER_PROXY_URL})' 'get?folder=neardet2x2.elifetime&t=0'",
+    # )
+    # self.client.pd_2x2_fnfileget_query(
+    #     f"{FN_FILEGET_PATH} -c '(serverurl={FRONTIER_SERVER_URL})(proxyurl={FRONTIER_PROXY_URL})' 'get?folder=neardet2x2.vdrift&t=0'",
+    # )
